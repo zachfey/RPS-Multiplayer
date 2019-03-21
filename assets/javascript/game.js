c365a028f51fdcc4c7f1107d2215315c4c20afe4
ZachFey <zachary.fey@gmail.com>
2019-03-21 19:49:31 -0400

    got lost, gave up

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+import { WINDOW_WIDTH, WINDOW_HEIGHT } from './config'
+import Game from './state/Game'
+
+class App extends Phaser.Game {
+  constructor () {
+    super(WINDOW_WIDTH, WINDOW_HEIGHT, Phaser.AUTO)
+    this.state.add('Game', Game)
+    this.state.start('Game')
+  }
+}
+
+const SimpleGame = new App()
+
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+        firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+    ],
+    // tosUrl and privacyPolicyUrl accept either url string or a callback
+    // function.
+    // Terms of service url/callback.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url/callback.
+    privacyPolicyUrl: function () {
+        window.location.assign('<your-privacy-policy-url>');
+    }
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
